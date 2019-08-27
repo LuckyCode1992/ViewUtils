6cb9ab19dfc82352749ab3fb9c3cb0555bc7d236
2214576751@qq.com <hxl19920401>
2019-08-27 14:42:44 +0800

    最定义view 区域

--- app/src/main/java/com/justcode/hxl/viewutil/自定义控件/绘图基础/BasisView.kt
+++ app/src/main/java/com/justcode/hxl/viewutil/自定义控件/绘图基础/BasisView.kt
@@ -1,9 +1,7 @@
 package com.justcode.hxl.viewutil.自定义控件.绘图基础
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -32,17 +30,44 @@ class BasisView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
         //画布，绘制成品
         canvas.drawCircle(200f, 200f, 100f, paint)
 
+        //画点
         val paint1 = Paint()
         paint1.strokeWidth = 20f
         paint1.style = Paint.Style.STROKE
         paint1.color = Color.BLACK
-        canvas.drawPoint(200f,200f,paint1)
+        canvas.drawPoint(200f, 200f, paint1)
 
+        //画线
         val paint2 = Paint()
         paint2.strokeWidth
         paint2.style = Paint.Style.STROKE
         paint2.color = Color.WHITE
-        canvas.drawLine(200f,200f,300f,200f,paint2)
+        canvas.drawLine(200f, 200f, 300f, 200f, paint2)
+
+        //路径
+        val paint3 = Paint()
+        paint3.color = Color.GREEN
+        paint3.style = Paint.Style.STROKE
+        paint3.strokeWidth = 5f
+        val path = Path()
+        //设置路径起始点
+        path.moveTo(10f, 400f)
+        //画线
+        path.lineTo(10f, 500f)
+        //画线
+        path.lineTo(300f, 500f)
+        //闭环  自动连接成闭环
+        path.close()
+        canvas.drawPath(path, paint3)
+
+        //弧线路径
+        val path2 = Path()
+        path.moveTo(400f, 400f)
+        val recf = RectF(500f, 400f, 700f, 490f)
+        path2.arcTo(recf, 0f, 90f)
+//        canvas.drawRect(recf, paint3)
+        canvas.drawPath(path2, paint3)
+
 
     }
 
