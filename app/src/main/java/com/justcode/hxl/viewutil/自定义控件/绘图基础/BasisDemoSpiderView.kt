458a8a432c57c9625ab051cb7deae13e5ea8a86c
2214576751@qq.com <hxl19920401>
2019-08-29 15:35:48 +0800

    绘图基础——字体简单demo

--- app/src/main/java/com/justcode/hxl/viewutil/自定义控件/绘图基础/BasisDemoSpiderView.kt
+++ app/src/main/java/com/justcode/hxl/viewutil/自定义控件/绘图基础/BasisDemoSpiderView.kt
@@ -8,7 +8,7 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
-class BasisDemoView @JvmOverloads constructor(
+class BasisDemoSpiderView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     var radarPaint = Paint()
